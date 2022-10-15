6d3ca969d44b7e13bf97a1af8006125950760cd7
M4X1K02 <91785448+M4X1K02@users.noreply.github.com>
2022-10-15 11:07:26 +0200

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,43 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <espnow.h>
+ 
+// Structure example to receive data
+// Must match the sender structure
+typedef struct struct_message {
+    int16_t gear;
+// Create a struct_message called myData
+} struct_message;
+struct_message Gear;
+ 
+// Callback function that will be executed when data is received
+void OnDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len) {
+  memcpy(&Gear, incomingData, sizeof(Gear));
+  Serial.print("Bytes received: ");
+  Serial.print(len);
+  Serial.print(": ");
+  Serial.println(Gear.gear);
+}
+  
+void setup() {
+  // Initialize Serial Monitor
+  Serial.begin(115200);
+   
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+ 
+  // Init ESP-NOW
+  if (esp_now_init() != 0) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+   
+  // Once ESPNow is successfully Init, we will register for recv CB to
+  // get recv packer info
+  esp_now_set_self_role(ESP_NOW_ROLE_SLAVE);
+  esp_now_register_recv_cb(OnDataRecv);
+}
+ 
+void loop() {
+  delay(1000);
+}
